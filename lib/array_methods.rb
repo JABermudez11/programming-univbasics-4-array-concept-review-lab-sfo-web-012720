fe52ffd290d7649fbc2669ef2392db699863201f
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-15 11:07:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,5 +18,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-
+  min = 1
+  array.length.times do |index|
+    if array[index] < min
+      min = array[index]
+    end
+  end
+  max
 end
